003d718c296707a470e8be9933a5065a2d0e299e
Tommy Alhamra <tommy.alhamra@gmail.com>
2021-01-04 08:43:11 +0700

    list data

--- app.js
+++ app.js
@@ -8,20 +8,40 @@ const {
 
 const app = express()
 
+let forumData = [{
+  id: "1",
+  title: "cara belajar",
+  desc: "bagaimana cara belajar yang baik?"
+}, {
+  id: "2",
+  title: "apa sekarang",
+  desc: "sekarang harus belajar apa"
+}, {
+  id: "3",
+  title: "mulai dari mana",
+  desc: "saya bingung mulai dari mana"
+}, ]
+
 let schema = buildSchema(`
+  type Forum{
+    id: ID,
+    title: String,
+    desc: String
+  }
+
   type Query{
-    hello: String
+    forums: [Forum]
   }
 `)
 
-let root = {
-  hello: () => 'Hello World'
+let resolvers = {
+  forums: () => forumData
 }
 
 
 app.use('/graphql', graphqlHTTP({
   schema: schema,
-  rootValue: root,
+  rootValue: resolvers,
   graphiql: true,
 }))
 
